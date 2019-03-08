5933f279d6c4fc637a42ac73a0a8b6fc1b9c1f15
Kael <piskael03@gmail.com>
2019-03-08 16:18:15 -0500

    test the pipeline

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Layout from '../components/layout';
 import styled from 'styled-components';
-import { useSpring } from 'react-spring';
 
 const ProjectContainer = styled.div`
   display: flex;
@@ -11,34 +10,28 @@ const ProjectContainer = styled.div`
   background: #f0f0f0;
   flex: 1;
   height: 100%;
-  
- & > div {
-  will-change: transform, opacity;
-    position: absolute;
-    width: 100%;
-    height: 100%;
- }
+
 `;
 const ProjectPage = () => {
-  const [selected, setSelected] = useState(null);
-  const handleClick = (index) => {
-    setSelected(index === 2 ? 0: index);
-  };
-
-  const [props, set] = useSpring(() => ({
-    config: { mass: 7, tension: 600, friction: 250 },
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
-  }));
-
-  const render = () => {
-
-  };
+  // const [selected, setSelected] = useState(null);
+  // const handleClick = (index) => {
+  //   setSelected(index === 2 ? 0: index);
+  // };
+  //
+  // const [props, set] = useSpring(() => ({
+  //   config: { mass: 7, tension: 600, friction: 250 },
+  //   from: { opacity: 0 },
+  //   enter: { opacity: 1 },
+  //   leave: { opacity: 0 },
+  // }));
+  //
+  // const render = () => {
+  //
+  // };
   return (
     <Layout>
-      <ProjectContainer onClick={() => handleClick()}>
-        {render()}
+      <ProjectContainer>
+        Project
       </ProjectContainer>
     </Layout>
   );
